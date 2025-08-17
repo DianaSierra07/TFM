a4a52ca71b10898b0adee92613448ec92fff960b
DianaSierra07 <dianasierrae.58@gmail.com>
2025-08-16 22:43:24 -0500

    Pestañas Web

---
+++ hitos.js
@@ -0,0 +1,23 @@
+{
+"type": "FeatureCollection",
+"name": "Hitos",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": 1, "Lugar tur�": "Playas de la ciudad", "X": "10.404170", "Y": "-75.556005", "% Visitant": "71.4" }, "geometry": { "type": "Point", "coordinates": [ -75.556005, 10.40417 ] } },
+{ "type": "Feature", "properties": { "fid": 2, "Lugar tur�": "Centros comerciales", "X": "10.404150", "Y": "-75.554326", "% Visitant": "70.7" }, "geometry": { "type": "Point", "coordinates": [ -75.554326, 10.40415 ] } },
+{ "type": "Feature", "properties": { "fid": 3, "Lugar tur�": "Plazas del centro histórico", "X": "10.424588", "Y": "-75.552069", "% Visitant": "70.1" }, "geometry": { "type": "Point", "coordinates": [ -75.552069, 10.424588 ] } },
+{ "type": "Feature", "properties": { "fid": 4, "Lugar tur�": "Restaurantes del centro histórico", "X": "10.423989", "Y": "-75.552078", "% Visitant": "65.6" }, "geometry": { "type": "Point", "coordinates": [ -75.552078, 10.423989 ] } },
+{ "type": "Feature", "properties": { "fid": 5, "Lugar tur�": "Barrio Getsemaní", "X": "10.419957", "Y": "-75.546021", "% Visitant": "59.9" }, "geometry": { "type": "Point", "coordinates": [ -75.546021, 10.419957 ] } },
+{ "type": "Feature", "properties": { "fid": 6, "Lugar tur�": "Iglesia del centro histórico", "X": "10.424435", "Y": "-75.552100", "% Visitant": "54.8" }, "geometry": { "type": "Point", "coordinates": [ -75.5521, 10.424435 ] } },
+{ "type": "Feature", "properties": { "fid": 7, "Lugar tur�": "Castillo San Felipe de Barajas", "X": "10.422502", "Y": "-75.539269", "% Visitant": "48.3" }, "geometry": { "type": "Point", "coordinates": [ -75.539269, 10.422502 ] } },
+{ "type": "Feature", "properties": { "fid": 8, "Lugar tur�": "Playa Blanca / Barú", "X": "10.142749", "Y": "-75.684867", "% Visitant": "41.8" }, "geometry": { "type": "Point", "coordinates": [ -75.684867, 10.142749 ] } },
+{ "type": "Feature", "properties": { "fid": 9, "Lugar tur�": "Islas del Rosario", "X": "10.177072", "Y": "-75.771581", "% Visitant": "39.8" }, "geometry": { "type": "Point", "coordinates": [ -75.771581, 10.177072 ] } },
+{ "type": "Feature", "properties": { "fid": 10, "Lugar tur�": "Bar o discoteca en el centro histórico", "X": "10.423304", "Y": "-75.549454", "% Visitant": "34.7" }, "geometry": { "type": "Point", "coordinates": [ -75.549454, 10.423304 ] } },
+{ "type": "Feature", "properties": { "fid": 11, "Lugar tur�": "Las Bóvedas", "X": "10.430065", "Y": "-75.546534", "% Visitant": "29.6" }, "geometry": { "type": "Point", "coordinates": [ -75.546534, 10.430065 ] } },
+{ "type": "Feature", "properties": { "fid": 12, "Lugar tur�": "Museo del centro histórico", "X": "10.423142", "Y": "-75.551521", "% Visitant": "22.4" }, "geometry": { "type": "Point", "coordinates": [ -75.551521, 10.423142 ] } },
+{ "type": "Feature", "properties": { "fid": 13, "Lugar tur�": "El Cerro de la Popa", "X": "10.426088", "Y": "-75.524169", "% Visitant": "19.0" }, "geometry": { "type": "Point", "coordinates": [ -75.524169, 10.426088 ] } },
+{ "type": "Feature", "properties": { "fid": 14, "Lugar tur�": "Tour en “chiva”", "X": "10.422836", "Y": "-75.546384", "% Visitant": "18.7" }, "geometry": { "type": "Point", "coordinates": [ -75.546384, 10.422836 ] } },
+{ "type": "Feature", "properties": { "fid": 15, "Lugar tur�": "Coches jalados por caballos", "X": "10.423223", "Y": "-75.549416", "% Visitant": "10.9" }, "geometry": { "type": "Point", "coordinates": [ -75.549416, 10.423223 ] } },
+{ "type": "Feature", "properties": { "fid": 16, "Lugar tur�": "Isla de Bocachica", "X": "10.325678", "Y": "-75.578793", "% Visitant": "9.2" }, "geometry": { "type": "Point", "coordinates": [ -75.578793, 10.325678 ] } }
+]
+}
